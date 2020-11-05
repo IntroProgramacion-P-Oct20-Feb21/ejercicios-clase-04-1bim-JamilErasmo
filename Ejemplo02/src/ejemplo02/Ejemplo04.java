4e8a8c39331530b316a56d73adc05df104cfe0db
jamil erasmo <jequituizaca@utpl.edu.ec>
2020-11-04 19:12:48 -0500

    tarea completa

--- Ejemplo02/src/ejemplo02/Ejemplo04.java
+++ Ejemplo02/src/ejemplo02/Ejemplo04.java
@@ -3,12 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejemplo02;
-
+package ejemplo02; 
 /**
  *
  * @author Usuario iTC
  */
+import java.util.Scanner;
+
 public class Ejemplo04 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String nombres;
+        String apellidos;
+        String ciudad;
+        String pais;
+        String nacimiento;
+        System.out.println("Ingrese sus nombres");
+        nombres = entrada.nextLine();
+        
+        System.out.println("Ingrese sus apellidos");
+        apellidos = entrada.nextLine();
+        
+        System.out.println("Ingrese el nombre de la ciudad");
+        ciudad = entrada.nextLine();
+        
+        System.out.println("Ingrese el nombre del pais");
+        pais = entrada.nextLine();
+        
+        System.out.println("ingrese el año de nacimiento");
+        nacimiento = entrada.nextLine();
+        
+        System.out.printf("Datos del estudiante\n%s\n%s\n%s\n%s\n%s\n",
+                nombres,apellidos,ciudad,pais,nacimiento);
+        
+
+ } 
     
-}
+} 
+    
